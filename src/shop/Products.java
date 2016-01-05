604f62565245fb57dd214175cf09a32516474a8c
kamilkazmierczak <kamil.kazmierczak@hotmail.com>
2016-01-05 17:29:13 +0100

    +Cart

--- src/shop/Products.java
+++ src/shop/Products.java
@@ -9,6 +9,7 @@ import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.Objects;
 import javax.swing.JFrame;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -32,7 +33,8 @@ public class Products extends javax.swing.JPanel {
     private void addItems() {
         _items.add(new Item(1, "pendrive", 34, (float) 12.5, "Bardzo szybki"));
         _items.add(new Item(2, "monitor", 2, (float) 2500, "Znakomity"));
-
+        _items.add(new Item(3, "tv", 4, (float) 5000, "Duzy"));
+        
         _tableContent = new Object[_items.size()][];
         for (int i = 0; i < _items.size(); i++) {
             _tableContent[i] = ((Item) _items.get(i)).getItem();
@@ -40,41 +42,96 @@ public class Products extends javax.swing.JPanel {
 
         String[] ColumnNames = {"Id", "Nazwa", "Liczba sztuk", "Cena", "Opis"};
         jTable1 = new JTable(_tableContent, ColumnNames);
+        jTable1.removeColumn(jTable1.getColumnModel().getColumn(0));
     }
 
     private void initCart() {
         DefaultTableModel model = new DefaultTableModel();
         CartTable = new JTable(model);
+        model.addColumn("Id");
         model.addColumn("Nazwa");
         model.addColumn("Cena");
+        CartTable.removeColumn(CartTable.getColumnModel().getColumn(0));
+        
+        
     }
 
     private void addItemToCart(int tableRow) {
 
         DefaultTableModel model = (DefaultTableModel) CartTable.getModel();
 
-        Integer id = (Integer) jTable1.getValueAt(tableRow, 0);
+        Integer id = (Integer) jTable1.getModel().getValueAt(tableRow, 0);
+        //Integer id = (Integer) jTable1.getValueAt(tableRow, 0);
 
         for (int i = 0; i < _items.size(); i++) {
             if (((Item) _items.get(i)).getId_towaru() == id) {
 
                 boolean already = false;
-
-                //int position = 0;
                 for (int j = 0; j < _cartContent.size(); j++) {
                     if (_cartContent.get(j).getId_towaru() == id) {
                         already = true;
-                        //position = j;
                     }
                 }
 
                 if (!already) {
+                    //System.out.println("WTF"+((Item)_items.get(i)).getNazwa());
                     _cartContent.add((Item) _items.get(i));
-                    model.addRow(new Object[]{((Item) _items.get(i)).getNazwa(), ((Item) _items.get(i)).getCena()});
+                    //System.out.println("WTF COUNT "+ _cartContent.size());
+                    model.addRow(new Object[]{_cartContent.get(_cartContent.size()-1).getId_towaru(),_cartContent.get(_cartContent.size()-1).getNazwa(),_cartContent.get(_cartContent.size()-1).getCena()});
+                    //model.addRow(new Object[]{((Item) _items.get(i)).getId_towaru(),((Item) _items.get(i)).getNazwa(), ((Item) _items.get(i)).getCena()});
                 }
             }
         }
     }
+    
+    
+    public void clearCartTable(final DefaultTableModel model) {
+    for( int i = model.getRowCount() - 1; i >= 0; i-- ) {
+        model.removeRow(i);
+    }
+        model.setColumnCount(0);
+        model.addColumn("Id");
+        model.addColumn("Nazwa");
+        model.addColumn("Cena");
+        CartTable.removeColumn(CartTable.getColumnModel().getColumn(0));
+    }
+    
+    
+    
+     private void deleteItemFromCart(int tableRow) {
+
+        DefaultTableModel model = (DefaultTableModel) CartTable.getModel();
+        //model.removeRow(tableRow);
+        
+        
+        Integer id = (Integer) CartTable.getModel().getValueAt(tableRow, 0);
+        
+            for (int i = 0; i < _cartContent.size(); i++) {
+             if (Objects.equals(((Item) _cartContent.get(i)).getId_towaru(), id)) {
+             _cartContent.remove(i);
+             }
+         }
+
+        //model.setColumnCount(0);
+   
+        
+        clearCartTable(model); 
+
+        
+        
+        for(Item item : _cartContent)
+        {
+            model.addRow(new Object[]{item.getId_towaru(),item.getNazwa(),item.getCena()});
+            System.out.println("left "+item.getId_towaru()+" "+item.getNazwa()+" "+item.getCena());
+        }
+
+         System.out.println("id"+id);
+         
+     }
+    
+    
+    
+    
 
     public Products(JFrame frame) {
         initComponents();
@@ -94,7 +151,20 @@ public class Products extends javax.swing.JPanel {
                 Point p = me.getPoint();
                 int row = table.rowAtPoint(p);
                 if (me.getClickCount() == 2) {
+                    //row = jTable1.convertRowIndexToModel(row);
                     addItemToCart(row);
+                }
+            }
+        });
+        
+        CartTable.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent me) {
+                JTable table = (JTable) me.getSource();
+                Point p = me.getPoint();
+                int row = table.rowAtPoint(p);
+                if (me.getClickCount() == 2) {
+                    //row = CartTable.convertRowIndexToModel(row);
+                    deleteItemFromCart(row);
                 }
             }
         });
@@ -117,6 +187,8 @@ public class Products extends javax.swing.JPanel {
         CartTable = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
 
         jLabel1.setFont(new java.awt.Font("Sitka Small", 0, 48)); // NOI18N
         jLabel1.setText("Produkty");
@@ -160,6 +232,20 @@ public class Products extends javax.swing.JPanel {
             }
         });
 
+        jButton3.setText("jButton3");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton5.setText("jButton5");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -167,19 +253,29 @@ public class Products extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(132, 132, 132)
-                        .addComponent(jLabel1))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(132, 132, 132)
+                                .addComponent(jLabel1))
+                            .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(174, 174, 174)
+                                .addComponent(jButton3)
+                                .addGap(18, 18, 18)
+                                .addComponent(jButton5)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(50, 50, 50)
+                                .addComponent(jLabel2))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(33, 33, 33)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(39, 39, 39)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -196,7 +292,9 @@ public class Products extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3)
+                    .addComponent(jButton5))
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -205,11 +303,33 @@ public class Products extends javax.swing.JPanel {
         _frame.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+      
+        System.out.println("Zawartość koszyka:");
+        for (int i = 0; i < _cartContent.size(); i++) {
+
+            System.out.println(_cartContent.get(i).getId_towaru() + " " + _cartContent.get(i).getNazwa());
+
+        }
+        System.out.println("To juz wszysko:");
+
+
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+       for(Item item : _cartContent)
+        {
+            System.out.println(item.getNazwa());
+        }
+    }//GEN-LAST:event_jButton5ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable CartTable;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
